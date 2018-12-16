9893386d5a20b306f6d62067e5f1be2e44fb84b9
lingfeng3007 <yao_ps@126.com>
2018-12-16 20:51:28 +0800

    Update GitLearn.java
    test1111

--- learn-git/src/main/java/cn/domore/learn/GitLearn.java
+++ learn-git/src/main/java/cn/domore/learn/GitLearn.java
@@ -6,6 +6,6 @@ package cn.domore.learn;
 public class GitLearn {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world1111");
     }
 }
