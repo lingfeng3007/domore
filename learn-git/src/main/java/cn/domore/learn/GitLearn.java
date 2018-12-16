7d7a28ded3957f6c6059cc1e0164909f2b2f91ad
lingfeng <yao_ps@126.com>
2018-12-16 20:48:01 +0800

    main method

---
+++ learn-git/src/main/java/cn/domore/learn/GitLearn.java
@@ -0,0 +1,11 @@
+package cn.domore.learn;
+
+/**
+ * Created by Administrator on 2018/12/16 0016.
+ */
+public class GitLearn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
